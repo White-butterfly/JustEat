2ace2cae6a6274aa0d87c5a9aadc9caa617c8810
muna <munaahmed212@yahoo.com>
2022-04-03 14:59:22 +0300

    preparing files and folders and the design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from 'react';
 
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Just Eat Rest</h1>
-    </div>
-  );
-}
+const App = () => (
+
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+  
+);
 
 export default App;
